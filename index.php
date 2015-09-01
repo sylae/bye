e4a2debae9846bce0679ded27aab799e71bef22a
Sylae Corell <sylae@calref.net>
2015-09-01 06:20:42 -0600

    import initial code
    bootstrap and jquery stuff (c) whoever owns it now

---
+++ index.php
@@ -0,0 +1,74 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>Standings</title>
+    <script src="/js/jquery.min.js" type="text/javascript" charset="UTF-8"></script>
+    <link href="/css/bootstrap.css" rel="stylesheet" />
+    <script src="/js/bootstrap.min.js"></script>
+    <meta charset="UTF-8" />
+  </head>
+  <body style="background:transparent;">
+    <div class="container">
+      <div class="panel panel-default" style="background: rgba(0,0,0,0);">
+        <table class="table" style="background: url('/img/rocket_bg.png') no-repeat scroll right top, url('/img/stripe.png') no-repeat scroll left top, rgba(21, 21, 21, 0.7);">
+          <thead>
+            <tr>
+              <th width="100%">STANDINGS</th>
+              <th>W</th>
+              <th>L</th>
+              <th>Pct</th>
+              <th>GB</th>
+              <th>History</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td>The Gray Fellowship</td>
+              <td>60</td>
+              <td>22</td>
+              <td>.732</td>
+              <td>0</td>
+              <td>5-5</td>
+            </tr>
+
+            <tr>
+              <td>The Council of the Wand</td>
+              <td>39</td>
+              <td>43</td>
+              <td>.476</td>
+              <td>21</td>
+              <td>4-6</td>
+            </tr>
+
+            <tr>
+              <td>The Emerald Circle</td>
+              <td>37</td>
+              <td>45</td>
+              <td>.451</td>
+              <td>22</td>
+              <td>3-7</td>
+            </tr>
+
+            <tr>
+              <td>Sarry's Order</td>
+              <td>37</td>
+              <td>45</td>
+              <td>.451</td>
+              <td>23</td>
+              <td>7-3</td>
+            </tr>
+
+            <tr>
+              <td>Iffin's Company</td>
+              <td>15</td>
+              <td>67</td>
+              <td>.183</td>
+              <td>45</td>
+              <td>0-10</td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+    </div>
+  </body>
+</html>
