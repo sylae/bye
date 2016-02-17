8756abd277f54d7f3f4b9c9243a36e0d1344b006
Sylae Corell <sylae@calref.net>
2016-02-16 22:54:46 -0700

    remove unused config stuff

--- config.sample.php
+++ config.sample.php
@@ -10,14 +10,3 @@ $config['challonge_api'] = 'ddce269a1e3d054cae349621c198dd52';
 
 // Tournament ID (either API id or the URL bit)
 $config['challonge_id'] = 'sample';
-
-// if true, put a "powered by challonge" at the bottom of the module.
-$config['challonge_expose'] = array(
-  'bracket' => true,
-  'standings' => false,
-  'scorebug' => false,
-);
-
-// settings for Twitch integration on the dashboard
-$config['twitch_clientid'] = "ddce269a1e3d054cae349621c198dd52"; 
-$config['twitch_channel'] = "sample";
