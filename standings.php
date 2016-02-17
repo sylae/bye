2dc74e1fa44cae8dfef78dec87d89f38948886bd
Sylae Corell <sylae@calref.net>
2016-02-16 22:51:10 -0700

    json output for standings

--- standings.php
+++ standings.php
@@ -1,11 +1,11 @@
 <?php
+
 /**
  * Display a table showing standings for the teams. Useful for round-robin.
  *
  * @license http://www.gnu.org/licenses/gpl-3.0.txt GNU General Public License 3
  * @author Sylae Jiendra Corell <sylae@calref.net>
  */
-
 require_once 'config.php';
 require_once 'sorts.php';
 
@@ -89,71 +89,6 @@ foreach ($teams as $id => $data) {
   $teams[$id]['diff'] = $teams[$id]['pts'] - $teams[$id]['ptsgiven'];
 }
 uasort($teams, 'sortStanding');
-?>
-<!DOCTYPE html>
-<html>
-  <head>
-    <title>Standings</title>
-    <script src="/js/jquery.min.js" type="text/javascript" charset="UTF-8"></script>
-    <link href="/css/bootstrap.css" rel="stylesheet" />
-    <script src="/js/bootstrap.min.js"></script>
-    <meta charset="UTF-8" />
-    <script>
-      $(document).ready(function() {
-        $(".panel").click(function() {
-          if ($(this).css('opacity') == 0) {
-            $(this).css('opacity', 1);
-          } else {
-            $(this).css('opacity', 0);
-          }
-        });
-        setTimeout(function() {
-          $(".panel").css('opacity', 1);
-        }, 1000);
-      });
-    </script>
-  </head>
-  <body style="background:transparent;">
-    <div class="container">
-      <div class="panel panel-default navbar-fixed-bottom" style="background: rgba(0,0,0,0);transition: opacity 1s ease-in-out; opacity:0;">
-        <table class="table" style="background: url('/img/rocket_bg.png') no-repeat scroll right top, url('/img/stripe.png') no-repeat scroll left top, rgba(21, 21, 21, 0.7);">
-          <thead>
-            <tr>
-              <th width="100%">STANDINGS</th>
-              <th style="text-align: center;">W</th>
-              <th style="text-align: center;">L</th>
-              <th style="text-align: center;">Pct</th>
-              <th style="text-align: center;">Sets</th>
-              <th style="text-align: center;">Goals</th>
-              <th style="text-align: center;">Diff</th>
-            </tr>
-          </thead>
-          <tbody style="text-align: center;">
-            <?php
-            foreach ($teams as $id => $data) {
-              echo
-              <<<STUFF
-            <tr>
-              <td style="text-align: left;">{$data['name']}</td>
-              <td>{$data['w']}</td>
-              <td>{$data['l']}</td>
-              <td>{$data['pct']}</td>
-              <td>{$data['sets']}</td>
-              <td>{$data['pts']}</td>
-              <td>{$data['diff']}</td>
-            </tr>
-STUFF;
-            }
-            ?>
-          </tbody>
-        </table><?php
-        if ($config['challonge_expose']['standings']) {
-          echo <<<CHA
-        <div style="background: rgba(21, 21, 21, 0.7);"><div class="panel-footer" style="background: rgba(21, 21, 21, 0.7);"><span class="pull-right"><small>Powered by Challonge <img src="img/challonge.png" style="height:1em" /></small></span><br /></div></div>
-CHA;
-        }
-        ?>
-      </div>
-    </div>
-  </body>
-</html>
+
+header("Content-Type: application/json; charset=utf-8");
+echo json_encode($teams);
