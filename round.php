a51f4a896ce031b6619da7ba343b6818dc2ff447
Sylae Corell <sylae@calref.net>
2016-03-09 01:17:52 -0700

    replace round list with json

--- round.php
+++ round.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * Display a table showing current round games. Good for round-robin
  *
@@ -18,10 +19,19 @@ $data = json_decode(file_get_contents(
     ), true)['tournament'];
 $matches = array();
 
-if (is_numeric($_GET['round'])) {
+if (array_key_exists('round', $_GET) && is_numeric($_GET['round'])) {
   $r = (int) $_GET['round'];
 } else {
-  die();
+  $matchsumm = [];
+  foreach ($data['matches'] as $i => $payload) {
+    if ($payload['match']['state'] == 'open') {
+      if (!array_key_exists($payload['match']['round'], $matchsumm)) {
+        $matchsumm[$payload['match']['round']] = 0;
+      }
+      $matchsumm[$payload['match']['round']] ++;
+    }
+  }
+  $r = array_keys($matchsumm, max($matchsumm))[0];
 }
 
 foreach ($data['matches'] as $i => $payload) {
@@ -60,70 +70,29 @@ foreach ($data['matches'] as $i => $payload) {
     }
   }
 }
-?>
-<!DOCTYPE html>
-<html>
-  <head>
-    <title>Round <?php echo $r; ?></title>
-    <script src="/js/jquery.min.js" type="text/javascript" charset="UTF-8"></script>
-    <link href="/css/bootstrap.css" rel="stylesheet" />
-    <script src="/js/bootstrap.min.js"></script>
-    <meta charset="UTF-8" />
-    <script>
-      $(document).ready(function() {
-        $(".panel").click(function() {
-          if ($(this).css('opacity') == 0) {
-            $(this).css('opacity', 1);
-          } else {
-            $(this).css('opacity', 0);
-          }
-        });
-        setTimeout(function() {
-          $(".panel").css('opacity', 1);
-        }, 1000);
-      });
-    </script>
-  </head>
-  <body style="background:transparent;">
-    <div class="container">
-      <div class="panel panel-default navbar-fixed-bottom" style="background: rgba(0,0,0,0);transition: opacity 1s ease-in-out; opacity:0;">
-        <table class="table" style="background: url('/img/rocket_bg.png') no-repeat scroll right top, url('/img/stripe.png') no-repeat scroll left top, rgba(21, 21, 21, 0.7);">
-          <thead>
-            <tr>
-              <th width="100%" colspan="3"><small><small style="text-transform: uppercase;"><?php echo $data['name']; ?></small></small><br /><h5>Round <?php echo $r; ?></h5></th>
-          </tr>
-          </thead>
-          <tbody style="text-align: center;">
-            <?php
-            foreach ($matches as $id => $data) {
-              $roundl = "";
-              if ($data['state'] == "complete" && $data['p1s'] + $data['p2s'] > 0) {
-                $scorebug = "{$data['p1s']} - {$data['p2s']}";
-              } elseif ($data['state'] != "complete" && ($data['p1s'] + $data['p2s'] > 0)) {
-                $scorebug = "{$data['p1s']} - {$data['p2s']}";
-                $roundl = " - In Progress";
-              } else {
-                $scorebug = "<small>MATCH PENDING</small>";
-              }
-              echo
-              <<<STUFF
-            <tr>
-              <td style="width:40%;text-align: right;"><h4>{$data['p1']}</h4></td>
-              <td><small><small style="text-transform: uppercase;">Match {$data['identifier']}{$roundl}</small></small><br /><h5>{$scorebug}</h5></td>
-              <td style="width:40%;text-align: left;"><h4>{$data['p2']}</h4></td>
-            </tr>
-STUFF;
-            }
-            ?>
-          </tbody>
-        </table><?php
-        if ($config['challonge_expose']['standings']) {
-          echo <<<CHA
-        <div style="background: rgba(21, 21, 21, 0.7);"><div class="panel-footer" style="background: rgba(21, 21, 21, 0.7);"><span class="pull-right"><small>Powered by Challonge <img src="img/challonge.png" style="height:1em" /></small></span><br /></div></div>
-CHA;
-        }
-        ?>
-      </div>
-    </div>
-  </body>
-</html>
+
+$payload = [
+  'tname' => $data['name'],
+  'round' => "Round $r",
+  'teams' => [],
+];
+
+foreach ($matches as $id => $data) {
+  $roundl = "";
+  if ($data['state'] == "complete" && $data['p1s'] + $data['p2s'] > 0) {
+    $scorebug = "{$data['p1s']} - {$data['p2s']}";
+  } elseif ($data['state'] != "complete" && ($data['p1s'] + $data['p2s'] > 0)) {
+    $scorebug = "{$data['p1s']} - {$data['p2s']}";
+    $roundl = " - In Progress";
+  } else {
+    $scorebug = "<small>MATCH PENDING</small>";
+  }
+  $payload['teams'][] = [
+    'p1' => $data['p1'],
+    'p2' => $data['p2'],
+    'match' => "Match " . $data['identifier'] . $roundl,
+    'score' => $scorebug,
+  ];
+}
+header("Content-Type: application/json; charset=utf-8");
+echo json_encode($payload);
